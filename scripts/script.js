02e89cdcaf1a87441179ed8eabd0c0e2a539b532
Bohdan Batsenko <bohdanbatsenko@Bohdans-MacBook-Pro.local>
2025-12-15 11:39:23 +0200

    add font size switcher

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,33 @@ menuLinks.forEach(link => {
     }, 100);
   });
 });
+
+
+
+const affectedElements = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6");
+
+affectedElements.forEach(function(element) {
+  element.dataset.origSize = window.getComputedStyle(element).fontSize;
+});
+
+document.querySelector('.js-increase').addEventListener("click", function() {
+  changeFontSize(1);
+});
+
+document.querySelector('.js-decrease').addEventListener("click", function() {
+  changeFontSize(-1);
+});
+
+document.querySelector('.js-origin').addEventListener("click", function() {
+  affectedElements.forEach(function(element) {
+    element.style.fontSize = element.dataset.origSize;
+  });
+});
+
+function changeFontSize(direction) {
+  affectedElements.forEach(function(element) {
+    const currentSize = parseInt(window.getComputedStyle(element).fontSize);
+    element.style.fontSize = (currentSize + direction) + "px";
+  });
+}
+
